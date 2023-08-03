11713490bce60b221c3430d31fe94100172845e7
Anthony Höhn <75019251+anthohn@users.noreply.github.com>
2023-08-03 10:50:52 +0200

    controllers ok

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 use Symfony\Component\HttpFoundation\Request;
 
 /*
@@ -20,45 +21,13 @@ Route::get('/', function () {
 });
 
 //prefix url + name
-Route::prefix('/blog')->name('blog.')->group(function() {
-    Route::get('/', function (Request $request){
-
-        //return error 44 if not found
-        // $posts = \App\Models\Post::findOrFail(22);
-
-        //return max 22 post
-        // $posts = \App\Models\Post::paginate(22);
-
-        // edit
-        // $post = \App\Models\Post::find(1);
-        // $post->title = 'Nouveau titre 1';
-        // $post->save();
-
-        // delete
-        // $post = \App\Models\Post::find(1);
-        // $post->delete();
-
-
-
-        dd($post);
-
-        return [
-            'link' => \route('blog.show', ['slug' => 'article', 'id' => 13])
-        ];
-    })->name('index');
+Route::prefix('/blog')->name('blog.')->controller(BlogController::class)->group(function() {
+    Route::get('/', 'index')->name('index');
     
     // URl with param
-    Route::get('/{slug}-{id}', function(string $slug, string $id, Request $request) {
-        return [
-            'slug' => $slug,
-            'id' => $id,
-            'name' => $request->input('name')
-        ];
-        //constraints on param
-    })->where([
+    Route::get('/{slug}-{id}', [BlogController::class, 'show'])->where([
         'id' => '[0-9]+',
         'slug' => '[a-z0-9\-]+'
     ])->name('show');
-
 });
 
